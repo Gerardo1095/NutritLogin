9f083aae378fe24c010f9c8081d922b7d88f3f1f
Gerardo Mendoza <gerardo_mendoza95@hotmail.com>
2020-04-06 19:14:01 -0700

    interface files created MVC

---
+++ NutritLogin/User Interface/Login/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  NutritLogin
+//
+//  Created by Gerardo Mendoza Avas on 06/04/20.
+//  Copyright © 2020 Gerardo Mendoza. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
